2453b12a9ad73c31d55158fd12d35267a06cd327
boring-plans <boring-plans@users.noreply.github.com>
2022-11-27 05:33:44 +0000

    deploy: 483a662ecab35102d4a1aac8a31caf051c2918fe

---
+++ assets/FineWeatherGallery.832fa571.js
@@ -0,0 +1,1 @@
+import{s as o,j as n,e as c,f as s,k as e,l as a,m as r,c as _}from"./index.70c58ba2.js";const l={},i={class:"container"},d=_("br",null,null,-1);function u(f,m){const t=n("RouterLink");return c(),s("div",i,[e(" Under construction :/ "),d,a(t,{to:"/"},{default:r(()=>[e("Back")]),_:1})])}const p=o(l,[["render",u]]);export{p as default};
