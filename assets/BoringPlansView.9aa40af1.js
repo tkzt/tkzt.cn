5e1f6eae195b2a84c07a91d2aa9da58a39540cb1
boring-plans <boring-plans@users.noreply.github.com>
2022-12-01 15:06:16 +0000

    deploy: 95bceff12d46eb846184dd1626322db3e20ca7ca

--- assets/BoringPlansView.9aa40af1.js
+++
@@ -1,1 +0,0 @@
-import{j as o,k as n,e as c,f as r,l as e,m as s,q as a,c as l}from"./index.0f945850.js";const _={},i=l("br",null,null,-1);function d(u,f){const t=n("RouterLink");return c(),r("div",null,[e(" Under construction :) "),i,s(t,{to:"/"},{default:a(()=>[e("Back")]),_:1})])}const k=o(_,[["render",d]]);export{k as default};
