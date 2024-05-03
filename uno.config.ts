5345561b927c24afa39435c321c015bfce7dc96e
Allen Tao <kang1.tao@tcl.com>
2024-05-03 14:46:44 +0800

    feat: adds unocss transformerVariantGroup

--- uno.config.ts
+++ uno.config.ts
@@ -1,11 +1,13 @@
 import { defineConfig } from 'unocss'
 import transformerDirectives from '@unocss/transformer-directives'
+import transformerVariantGroup from '@unocss/transformer-variant-group'
 import { presetUno } from 'unocss'
 import presetIcons from '@unocss/preset-icons'
 
 export default defineConfig({
   transformers: [
     transformerDirectives(),
+    transformerVariantGroup()
   ],
   presets: [presetUno(), presetIcons()],
 })
