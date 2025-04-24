208a49b3cfe4668dd6d7c5e22c76c0123b79dab4
Allen Tao <allen@tkzt.cn>
2025-04-24 17:07:56 +0800

    feat: multiple emoji-reaction keys support

--- server/api/reactions/index.ts
+++ server/api/reactions/index.ts
@@ -1,10 +1,12 @@
 import { emojis } from '~~/shared/constants'
 
-export default defineEventHandler(async () => {
+export default defineEventHandler(async (event) => {
+  let { prefix } = getQuery(event)
+  prefix = prefix?.toString() || ''
   const dataStorage = useStorage('data')
 
   const reactions = await Promise.all(
-    emojis.map(async (emoji) => ({ reactors: (await dataStorage.getItem(emoji))?.toString().split(',') || [], reaction: emoji }))
+    emojis.map(async (emoji) => ({ reactors: (await dataStorage.getItem(`${prefix ? prefix + '-' : ''}${emoji}`))?.toString().split(',') || [], reaction: emoji }))
   )
   return reactions.filter(r => !!r.reactors.length)
 })
